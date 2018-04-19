44f932657038c02e124a9c077f3d9340433d24cc
Jah <tess9roses@gmail.com>
2018-04-20 00:37:17 +0300

    Added new reward item,  beautify code a little, back to 180 sec pars

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const key = require('../key.js'); // bot private key and other stuff
 
-const refreshRate = 20 * 1000; // data fetching frequency
+const refreshRate = 180 * 1000; // data fetching frequency
 
 var state, alertItem = {}, alertId = [];
 
@@ -37,23 +37,24 @@ var missionReward = {
     '/Lotus/Types/Items/MiscItems/Alertium':        'Нитаин',
     '/Lotus/Types/Items/MiscItems/Gallium':         'Галлий',
     '/Lotus/Types/Items/MiscItems/VoidTearDrop':    'Отголоски бездны',
+    '/Lotus/Types/Items/MiscItems/Eventium':        'Синтула',
 }
 
 function alerts() {
-    var request = http.request(options, function (res) {
+    var request = http.request( options, function (res) {
         var data = '';
-        res.on('data', function (chunk) {
+        res.on('data', function ( chunk ) {
             data += chunk;
         });
         res.on('end', function () {
-            state = JSON.parse(data);
-            console.log('Complite fetching.');
-            setTimeout(alerts, refreshRate);
+            state = JSON.parse( data );
+            console.log( 'Complite fetching.' );
+            setTimeout( alerts, refreshRate );
             parse();
         });
     });
     request.on('error', function (e) {
-        console.log(e.message);
+        console.log( e.message );
     });
 
     request.end();
@@ -80,27 +81,28 @@ function reward(r) {
     if ( missionReward[r] ) {
         return missionReward[r];
     } else {
+        bot.channels.get(key.data.channel_id()).send('Неизвестная награда, шеф! ' + r); // Temporarily
         return r;
     }
 }
 
 function msToTime(s) {
     var ms = s % 1000;
-    s = (s - ms) / 1000;
+    s = ( s - ms ) / 1000;
     var sec = s % 60;
-    s = (s - sec) / 60;
+    s = ( s - sec ) / 60;
     var min = s % 60;
     var hr = (s - min) / 60;
 
-    if (hr != 0) {
+    if ( hr != 0 ) {
         return hr + 'ч ' + min + 'м ' + sec +'с'
     } else {
-        if (sec <= 0 && min == 0) {
+        if ( min <= 0 && sec <= 0) {
             return 'expired'
         } else {
             return min + 'м ' + sec +'с'
         }
-        
+
     }
     
 }
@@ -116,46 +118,45 @@ function itemCheck(i) {
 function parse() {
     alertItem = [];
     var temp = [];
-    for (i = 0; i < state['Alerts'].length; i++) {
+    for ( i = 0; i < state['Alerts'].length; i++ ) {
         var oid = state['Alerts'][i]['_id']['$oid'],
             expire = state['Alerts'][i]['Expiry']['$date']['$numberLong'],
-            missionType = mission(state['Alerts'][i]['MissionInfo']['missionType']),
-            enemy = faction(state['Alerts'][i]['MissionInfo']['faction']);
+            missionType = mission( state['Alerts'][i]['MissionInfo']['missionType'] ),
+            enemy = faction( state['Alerts'][i]['MissionInfo']['faction'] );
             lvl = state['Alerts'][i]['MissionInfo']['minEnemyLevel'] + '-' + state['Alerts'][i]['MissionInfo']['maxEnemyLevel'] + '   ',
-            itemsTemp = reward(state['Alerts'][i]['MissionInfo']['missionReward']['countedItems']),
+            itemsTemp = reward( state['Alerts'][i]['MissionInfo']['missionReward']['countedItems'] ),
             items = [];
             
         if ( itemsTemp && itemsTemp.length > 0 ) {
             items = reward( itemsTemp[0]['ItemType'] );
         }
 
-        temp.push( oid, missionType, enemy, lvl, expire, itemCheck(items) );
+        temp.push( oid, missionType, enemy, lvl, expire, itemCheck( items ) );
         alertItem.push( temp );
         temp = [];
     }
 }
-//bot.channels.get(key.data.channel_id()).send('Шеф! Нитаин завезли');
 
-bot.on('message', (message) => {
-    if (message.content == '-h') {
+bot.on( 'message', ( message ) => {
+    if ( message.content == '-h' ) {
         message.reply('```Дороу!\nЯ - ВафлБот.\nИ вот что ты можешь попросить меня сделать, дорогой друг.\nТы можешь набрать:\n-l       : и я покажу, чо щас в алертах\n\n\nmore commands in development...\nmaybe...```');
-    } else if (message.content == 'meh') {
+    } else if ( message.content == 'meh' ) {
         message.reply('слышь, жуёба, хватит лениться!');
-    } else if (message.content == '-l') {
+    } else if ( message.content == '-l' ) {
         let content = '';
 
         if ( alertItem.length > 0 ) {
             var serverTime = new Date();
-            alertItem.forEach(function(item){
+            alertItem.forEach( function( item ) {
                 content += '```' + item[1] + ' ' + item[2] + ' ' + item[3] + ' осталось: ' + msToTime(item[4] - serverTime) + '   ' + item[5] + '```';
             });
-            message.reply(content);
+            message.reply( content );
         } else {
             message.reply('```Fetching data from Warframe. Please try again later (in 5-10 seconds)```');
         }
     }
 });
 
-bot.login(key.data.bot_id());
+bot.login( key.data.bot_id() );
 
 alerts();
